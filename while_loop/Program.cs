c97990467889aaaaa225d0ea97431e9c84314c6f
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-23 21:40:47 +1100

    First commit

---
+++ while_loop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace while_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 5;
+
+            while (counter > 0)
+            {
+                Console.WriteLine("Counter = {0}", counter);
+                counter = counter - 1;
+            }
+        }
+    }
+}
